dfcf0fc89621eed9edd8eed13778c24cbf8153c8
wyumhn <tacoonobslr@gmail.com>
2025-05-23 17:33:51 +0900

    add pm2 to docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,13 +5,16 @@ set -e
 source /opt/ros/humble/setup.bash
 source /root/ros2_ws/install/setup.bash
 
-# WebSocketサーバ 起動（バックグラウンド）
+# pm2 プロセスに登録
 cd /root/web/server
-node app.js &
+pm2 start app.js --name websocket-server
 
-# 必要ならクライアントも起動（ポート3001）
 cd /root/web/client
-npm run start &
+pm2 start "npm run start" --name web-client --interpreter bash
 
-# ROS2 ノード起動（フォアグラウンド）
-python3 -m gnss_bridge.gnss_bridge
+# ROS2ノード
+cd /root/ros2_ws
+pm2 start "python3 -m gnss_bridge.gnss_bridge" --name gnss-bridge --interpreter bash
+
+# コンテナが終了しないようにフォアグラウンドで pm2 を保持
+pm2 logs
