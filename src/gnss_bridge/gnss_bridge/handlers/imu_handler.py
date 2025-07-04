a0a60ef1eb03793690e4ec423473409570a355f9
wyumhn <tacoonobslr@gmail.com>
2025-07-04 15:01:20 +0900

    add imu handler

--- src/gnss_bridge/gnss_bridge/handlers/imu_handler.py
+++ src/gnss_bridge/gnss_bridge/handlers/imu_handler.py
@@ -1,3 +1,4 @@
+import math
 from gnss_bridge.data_handler import DataHandler
 from sensor_msgs.msg import Imu
 
@@ -9,15 +10,33 @@ class ImuHandler(DataHandler):
         """
         Imuメッセージから角速度と線形加速度を抽出し、指定された形式の辞書を返す
         """
+        # クォータニオンの各成分を取得
+        x = msg.orientation.x
+        y = msg.orientation.y
+        z = msg.orientation.z
+        w = msg.orientation.w
+
+        # オイラー角（ロール、ピッチ、ヨー）
+        # ロール (x軸)
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+
+        # ピッチ (y軸)
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+
+        # ヨー (z軸)
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+
         return {
-            "angular_velocity": {
-                "x": msg.angular_velocity.x,
-                "y": msg.angular_velocity.y,
-                "z": msg.angular_velocity.z
-            },
-            "linear_acceleration": {
-                "x": msg.linear_acceleration.x,
-                "y": msg.linear_acceleration.y,
-                "z": msg.linear_acceleration.z
+            "euler_angles": {
+                "roll": roll_x,
+                "pitch": pitch_y,
+                "yaw": yaw_z
             }
         }
