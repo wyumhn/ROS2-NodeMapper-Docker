3c0ce8642af2e3eb9c22b5131a0b7120393e451d
wyumhn <tacoonobslr@gmail.com>
2025-07-20 00:11:10 +0900

    fixed mic

--- src/gnss_bridge/gnss_bridge/handlers/mic_handler.py
+++ src/gnss_bridge/gnss_bridge/handlers/mic_handler.py
@@ -1,13 +1,20 @@
 from gnss_bridge.data_handler import DataHandler
+from std_msgs.msg import Float32MultiArray
 
 class MicHandler(DataHandler):
-    """
-    特定のハンドラが定義されていないトピックに使用されるデフォルトのハンドラ
-    このハンドラはペイロードを処理せず、常に空の辞書を返す
-    """
-    def process(self, msg) -> dict:
-        """
-        どんなメッセージに対しても、空のペイロードを返す
-        最終的なトピック名は、この後メインノードのコールバックで付与される
-        """
-        return {}
+    def process(self, msg: Float32MultiArray) -> dict:
+        if len(msg.data) >= 8:
+            return {
+                "data1": msg.data[0],
+                "data2": msg.data[1],
+                "data3": msg.data[2],
+                "data4": msg.data[3],
+                "data5": msg.data[4],
+                "data6": msg.data[5],
+                "data7": msg.data[6],
+                "data8": msg.data[7]
+            }
+        else:
+            print("Warning: Float32MultiArray.data does not contain at least 8 elements.")
+            return {}
+
